858f60ea7602cf7d4b51710f2e5a7c4e67a67f56
Lorenzo Gucci <67734762+LorenzoGucci@users.noreply.github.com>
2020-08-12 13:40:35 +0100

    Update story_generator.py
    Added a loop to enable the user to generate another story or exit.

--- story_generator.py
+++ story_generator.py
@@ -4,15 +4,15 @@
 import random
 
 # define the various options and choose randomly one
-villain = random.choice(['Kyle Ren', 'Malloc', 'Darth Sebelius', 'Theranos', 'Lord Juul'])
-friend = random.choice(['Kym Spacemeasurer','Teen Yoda','Dab Tweetdek', 'Yaz Progestin', 'TI-83'])
-lightsaber = random.choice(['beige', 'ochre', 'mauve', 'aquamarine', 'taupe'])
-superweapon = random.choice(['Sun Obliterator', 'Moonsquisher', 'World Eater', 'Planet Zester', 'Superconducting Supercollider'])
-superpower = random.choice(['blowing up a planet with a bunch of beams of energy that combine into one', 'blowing up a bunch of planets with one beam of energy that splits into many', 'cutting a planet in half and smashing the halves together like two cymbals', "increasing the CO2 levels in a planet's atmosphere, causing rapid heating", 'triggering the end credits before the movies is done'])
-old_enemy = random.choice(['Boba Fett', 'Salacious Crumb', 'The Space Slug', 'The Bottom Half of Darth Maul', 'YouTube Commenters'])
-feat = random.choice(['a bow that shoots little lightsaber-headed arrows.', 'X-Wings and TIE Fighters dodging the giant letters of the opening crawl.', 'a Sith educational display that uses force lightning to demonstrate the dielectric breakdown of air.', 'Kylo Ren putting on another helmet over his smaller one.', 'a Sith car wash where the bristles on the brushes are little lightsabers.'])
-father = random.choice(['Luke', 'Leia', 'Han', 'Obi-Wan', 'a random junk-trader'])
-mother = random.choice(['Poe.', 'BB-8.', 'Amilyn Holdo.', 'Laura Dern.', 'a random junk-trader.', 'that one droid from the Jawa Sandcrawler that says "gonk".'])
+villain = ['Kyle Ren', 'Malloc', 'Darth Sebelius', 'Theranos', 'Lord Juul']
+friend = ['Kym Spacemeasurer','Teen Yoda','Dab Tweetdek', 'Yaz Progestin', 'TI-83']
+lightsaber = ['beige', 'ochre', 'mauve', 'aquamarine', 'taupe']
+superweapon = ['Sun Obliterator', 'Moonsquisher', 'World Eater', 'Planet Zester', 'Superconducting Supercollider']
+superpower = ['blowing up a planet with a bunch of beams of energy that combine into one', 'blowing up a bunch of planets with one beam of energy that splits into many', 'cutting a planet in half and smashing the halves together like two cymbals', "increasing the CO2 levels in a planet's atmosphere, causing rapid heating", 'triggering the end credits before the movies is done']
+old_enemy = ['Boba Fett', 'Salacious Crumb', 'The Space Slug', 'The Bottom Half of Darth Maul', 'YouTube Commenters']
+feat = ['a bow that shoots little lightsaber-headed arrows.', 'X-Wings and TIE Fighters dodging the giant letters of the opening crawl.', 'a Sith educational display that uses force lightning to demonstrate the dielectric breakdown of air.', 'Kylo Ren putting on another helmet over his smaller one.', 'a Sith car wash where the bristles on the brushes are little lightsabers.']
+father = ['Luke', 'Leia', 'Han', 'Obi-Wan', 'a random junk-trader']
+mother = ['Poe.', 'BB-8.', 'Amilyn Holdo.', 'Laura Dern.', 'a random junk-trader.', 'that one droid from the Jawa Sandcrawler that says "gonk".']
 
 # creates the parts of the story
 intro = 'In this Star Wars movie, our heroes return to take on the First Order and new villain '
@@ -26,4 +26,19 @@ part_7 = "\n\nP.S. Rey's parents are "
 part_8 = ' and '
 
 # generates the story
-print(intro + villain + part_1 + friend + part_2 + lightsaber + part_3 + superweapon + part_4 + superpower + part_5 + old_enemy + part_6 + feat + part_7 + father + part_8 + mother)
+def rsg():
+  print(intro + random.choice(villain) + part_1 + random.choice(friend) + part_2 + random.choice(lightsaber) + part_3 + random.choice(superweapon) + part_4 + random.choice(superpower) + part_5 + random.choice(old_enemy) + part_6 + random.choice(feat) + part_7 + random.choice(father) + part_8 + random.choice(mother))
+
+
+while True:
+  rsg()
+  while True:
+    user_input = input('Would you like to generate a new spoiler? [y,n]\n')
+    if user_input not in ('y', 'n'):
+      print('Please enter "y" for yes or "n" for no.')
+      continue
+    if user_input == 'y':
+      break
+    else:
+      print('Alright, ciao!')
+      quit()
